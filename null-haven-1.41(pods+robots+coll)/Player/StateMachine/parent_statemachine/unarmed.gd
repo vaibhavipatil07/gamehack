4724df501cfd1ff046b1c3d89fa90bf3a5416d18
vaibhavipatil07 <vaibhavijpatil07@gmail.com>
2026-04-10 12:20:27 +0530

    Delete null-haven-1.41(pods+robots+coll) directory

--- null-haven-1.41(pods+robots+coll)/Player/StateMachine/parent_statemachine/unarmed.gd
+++
@@ -1,9 +0,0 @@
-extends CombatTransitionState
-
-
-func _state_input(event: InputEvent) -> void:
-	if event.is_action_pressed("holster_weapon"):
-		finished.emit("Armed")
-
-func _on_camera_model_view_changed(view: bool) -> void:
-	state_machine._set_active(!view)
